d4cc7068ce9043b2cd2e327390dcd23b9a2034ad
dhruv7100 <dmunjani2@gmail.com>
2021-09-17 12:41:45 +0530

    updated Division functionality.

--- First/src/com/logilite/demo/Calculator.java
+++ First/src/com/logilite/demo/Calculator.java
@@ -26,7 +26,7 @@ public class Calculator {
 			ans = num1 * num2;
 			break;
 		case '/':
-			ans = num1 / num2;
+			ans = num1 * num2;
 			break;
 		default:
 			System.out.printf("Error! Invalid operator");
